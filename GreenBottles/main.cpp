ec62ed7ffacbe11473ca22f37db9e488d7033237
Ernesto Herrera Arias <ernestoha@gmail.com>
2020-06-08 01:30:16 -0400

    GreenBottles first commit

---
+++ GreenBottles/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //cout << "Hello world!" << endl;
+    int greenBottles = 10;
+    cout << "There were " << greenBottles << " green bottles" <<"\n";
+    greenBottles--;
+    cout << "There were " << greenBottles << " green bottles";
+    return 0;
+}
